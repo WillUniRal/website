bb6789bd18829352e8952460c13dac17e3a496f4
WillUniRal <147512158+WillUniRal@users.noreply.github.com>
2024-04-12 14:13:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+function onYouTubeIframeAPIReady() {
+  var player;
+  player = new YT.Player('player', {
+    videoId: 'M7lc1UVf-VE',
+    playerVars: { 'autoplay': 1, 'controls': 0 },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange,
+      'onError': onPlayerError
+    }
+  });
+}
