90429569bfd1380bdd8d75fe37bd1878260e3513
WillUniRal <147512158+WillUniRal@users.noreply.github.com>
2024-04-12 14:34:28 +0100

    rick

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
-
-jQuery(document).ready(function( $ ) {
-  $('.video-selector iframe')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-});
