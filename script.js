5758a8b12410785d0b11beacd6acb5ad24ce7f1c
WillUniRal <22300044@bucks.ac.uk>
2024-04-13 01:25:38 +0100

    hexagons

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+	document.getElementById('currentTime').innerHTML = new Date().toLocaleString();
 }
 showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+const container = document.querySelector('.container');
+const hexagonPattern = [13, 12, 13, 12, 13, 12, 13, 12, 13, 12];
+
+for (let i = 0; i < hexagonPattern.length; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < hexagonPattern[i]; j++) {
+        const hexagon = document.createElement('div');
+        hexagon.classList.add('hexagon');
+        // Add spacing between hexagons
+        hexagon.style.marginRight = '0px';
+        row.appendChild(hexagon);
+    }
+    container.appendChild(row);
+}
+function mouse_position() {
+    var e = window.event;
+
+    var posX = e.clientX;
+    var posY = e.clientY;
+
+    document.Form1.posx.value = posX;
+    document.Form1.posy.value = posY;
+
+    var t = setTimeout(mouse_position,100);
+
+}
