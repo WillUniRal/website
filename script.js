82cc2bccd893831a98dbc45e3d43f4825dcd164a
WillUniRal <147512158+WillUniRal@users.noreply.github.com>
2024-04-16 10:25:45 +0100

    hexagon location

--- script.js
+++ script.js
@@ -16,20 +16,78 @@ for (let i = 0; i < hexagonPattern.length; i++) {
         const hexagon = document.createElement('div');
         hexagon.classList.add('hexagon');
         // Add spacing between hexagons
+		
+		hexagon.id = `hexagon_${i}_${j}`;
+		
         hexagon.style.marginRight = '0px';
         row.appendChild(hexagon);
+		//const rect = hexagon.getBoundingClientRect();
+		//console.log("x:"+rect.left+"y:"+rect.top);
     }
     container.appendChild(row);
 }
-function mouse_position() {
-    var e = window.event;
+function getHexagonPositions() {
+    const hexagonPositions = [];
 
-    var posX = e.clientX;
-    var posY = e.clientY;
+    for (let i = 0; i < hexagonPattern.length; i++) {
+        for (let j = 0; j < hexagonPattern[i]; j++) {
+            const hexagonId = `hexagon_${i}_${j}`;
+            const hexagonElement = document.getElementById(hexagonId);
+            const rect = hexagonElement.getBoundingClientRect();
 
-    document.Form1.posx.value = posX;
-    document.Form1.posy.value = posY;
+            // Push the position of the hexagon to the array
+            hexagonPositions.push({
+                id: hexagonId,
+                x: rect.left,
+                y: rect.top
+            });
+        }
+    }
 
-    var t = setTimeout(mouse_position,100);
-
+    return hexagonPositions;
 }
+const positions = getHexagonPositions();
+console.log(positions);
+
+(function() {
+    var mousePos;
+
+    document.onmousemove = handleMouseMove;
+    setInterval(getMousePosition, 100); // setInterval repeats every X ms
+
+    function handleMouseMove(event) {
+        var dot, eventDoc, doc, body, pageX, pageY;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        mousePos = {
+            x: event.pageX,
+            y: event.pageY
+        };
+    }
+    function getMousePosition() {
+        var pos = mousePos;
+        if (!pos) {
+            // We haven't seen any movement yet
+        }
+        else {
+            //console.log(pos);
+        }
+    }
+})();
